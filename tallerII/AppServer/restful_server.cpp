07cb9a79295e68c33a5e8e560a48b0dc64d23f24
Agustin Linari <agustinlinari@gmail.com>
2016-03-20 21:52:30 -0300

    saco logs

--- tallerII/AppServer/restful_server.cpp
+++ tallerII/AppServer/restful_server.cpp
@@ -7,8 +7,7 @@
 #include <iostream>
 #include <assert.h>
 #include "rocksdb/db.h"
-#include <log4cpp/Category.hh>
-#include <log4cpp/PropertyConfigurator.hh>
+
 
 static const char *s_http_port = "3000";
 static struct mg_serve_http_opts s_http_server_opts;
@@ -59,34 +58,6 @@ int main(int argc, char *argv[]) {
 #ifdef MG_ENABLE_SSL
   const char *ssl_cert = NULL;
 #endif
-
-	std::string initFileName = "log4cpp.properties";
-	log4cpp::PropertyConfigurator::configure(initFileName);
-
-	log4cpp::Category& root = log4cpp::Category::getRoot();
-
-	log4cpp::Category& sub1 =
-		log4cpp::Category::getInstance(std::string("sub1"));
-
-	log4cpp::Category& sub2 =
-		log4cpp::Category::getInstance(std::string("sub1.sub2"));
-
-	root.warn("Warning example");
-
-	sub1.debug("Received storm warning");
-	sub1.info("Closing all hatches");
-
-	sub2.debug("Hiding solar panels");
-	sub2.error("Solar panels are blocked");
-	sub2.debug("Applying protective shield");
-	sub2.warn("Unfolding protective shield");
-	sub2.info("Solar panels are shielded");
-
-	sub1.info("All hatches closed");
-
-	root.info("Ready for storm.");
-
-	log4cpp::Category::shutdown();
 
   rocksdb::DB* db;
   rocksdb::Options options;
