1ad0c7fe6e0c9f2f8fcc306e9c8667315d4b0aa1
Agustin Linari <agustinlinari@gmail.com>
2016-06-17 13:52:53 -0300

    limpio archivos que no se usan

--- AppServer/test/authentication_user.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-r = requests.get("http://localhost:3000/login_user", data = {"name":"user","password":"1234"})
-
-if r.status_code == 200:
-    print r.text
--- AppServer/test/new_user.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-r = requests.post("http://localhost:3000/user/newuser", data = {"name":"user","password":"1234","firstName":"Pablo","lastName":"Sivori","email":"sivori.daniel@gmail.com"})
-
-if r.status_code == 200:
-    print r.text
